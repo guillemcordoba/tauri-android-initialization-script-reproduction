c49971a863b40f59d8c3ce065cb8437afa865ca8
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-09-20 14:23:34 +0200

    Code

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,27 @@
+use tauri::{scope::ipc::RemoteDomainAccessScope, Manager, WindowBuilder, WindowUrl};
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .setup(|app| {
+            app.ipc_scope().configure_remote_access(
+                RemoteDomainAccessScope::new(String::from("https://tauri.app"))
+                    .add_window(String::from("main")),
+            );
+
+            let window = WindowBuilder::new(
+                app,
+                String::from("main"),
+                WindowUrl::External(
+                    url::Url::parse("https://tauri.app").expect("Cannot parse url"),
+                ),
+            )
+            .initialization_script("console.error('hello from the webview!');")
+            .build()?;
+            window.eval("console.log(window.__TAURI_IPC__)")?;
+
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
